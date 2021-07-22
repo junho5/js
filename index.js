621fae0bbab080b365013e18417ddd2b4965dc31
junho5 <dhwnsghdhkd@naver.com>
2021-07-23 00:39:51 +0900

    function 2

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-function greetings(sentence){
-  console.log('Hi')
-  console.log('Hi')
-  console.log('Hi')
-  console.log('Hi')
-  console.log('Hi')
-  console.log('Hi')
-  console.log('Hi')
-  console.log('Hi')
-  console.log('sentence')
-  
-};
+function welcome(name){
+  console.log('안녕하세요'+name+'님');
+}
 
-greetings('HOla');
+welcome('junho')
+
+function add(a,b){
+  console.log(a+b);
+}
+add(2,5);
+
+function getTwo(){
+  return 2;
+}
+console.log(getTwo());
+
+function getTwice(number){
+  return number *2;
+}
+let x = getTwice(5);
+let y = getTwice(10);
+
+console.log(x*y);
