b0a4685004dafd7015909ab98d512d692f72bb55
junho5 <dhwnsghdhkd@naver.com>
2021-07-26 00:22:32 +0900

    boolean

--- index.js
+++ index.js
@@ -24,9 +24,13 @@ console.log(x*y);
 
 console.log(7%3);
 console.log(7**2);
-console.log('코드잇')
+console.log('코드잇');
 
-console.log("I'm student")
-console.log("I\'m student\"I\'m student")
-console.log(`I'm student I'm student`)
+console.log("I'm student");
+console.log("I\'m student\"I\'m student");
+console.log(`I'm student I'm student`);
+
+console.log(2>1);
+console.log(2<1);
+
 
