16f6d9f5be028bd06c7c194ccf7eb460f9f30ae1
junho5 <dhwnsghdhkd@naver.com>
2021-07-27 23:03:53 +0900

    type casting

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-console.log(typeof 101);
-console.log(typeof '101');
-console.log(typeof 101);
+console.log(4+'2');
+console.log(4+2);
+console.log(4-true);
+console.log(4*false);
+console.log(4/'2');
+console.log('4'**true);
+console.log(4%'two');
 
-let name ='codeit';
-function sayhello(){
-  console.log('sayhello');
-}
 
-console.log(typeof name);
-console.log(typeof sayhello);
+console.log(2<'3');
+console.log(2>true);
+console.log('2'<=false);
+console.log('two'>=1);
 
-console.log(typeof 'hello'+'codeit');
-console.log(typeof 8-4);
+
+// ===는 일치, 불일치 (형변환 x)
+// ==는 동등, 부등 (형변환)
+console.log(1==='1');
+console.log(1===true);
+console.log(1=='1');
+console.log(1==true);
