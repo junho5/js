aeadf9fb2a41ee80e0daa144ee0917c61be8a196
junho5 <dhwnsghdhkd@naver.com>
2021-07-29 00:26:28 +0900

    const

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-// Scope : 범위, 영역
-let x = 3; // 글로벌 변수, 전역 변수
+// 상수
+// 상수는 표현할때 대문자 , 두단어 이상은 _ 사용 
+// const PI , const MY_NUMBER
+const PI = 3.14
+// let PI = 3.14; // 원주율
+let radius = 0; // 반지름
 
-function myFunction(){ // 블록문
-  let x = 3; // 로컬변수, 지역변수
-  console.log(x);
+function calculateArea(){
+  return PI * radius * radius;
 }
 
-myFunction();
-console.log(x);
+function printArea(){
+  return `반지름이 ${radius}일 떄, 원의 넓이는 ${calculateArea()}`;
+}
+
+radius = 4 ;
+console.log(printArea());
+
+radius = 7 ;
+console.log(printArea());
+
+radius = 8 ;
+console.log(printArea());
