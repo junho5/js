71d78dfc4320c98c03448a7b85abeb889422c40d
junho5 <dhwnsghdhkd@naver.com>
2021-07-30 00:21:41 +0900

    for

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-// switch문
-let mychoice = 2;
+// 반복문 for문
 
-switch(mychoice){
-  case 1:
-    console.log('토끼');
-    break;
-  case 2:
-    console.log('호랑이');
-    break;
-  case 3:
-    console.log('강아지');
-    break;
-  case 4:
-    console.log('코알라');
-    break;
-  default:
-    console.log('1~4선택헤주세요');
+for (let i = 1; i<=10; i++){
+  console.log(`${i}번째`);
 }
+
