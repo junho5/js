dd9e96e7264ddc4b2bac31112fc36d2b7bc76110
junho5 <dhwnsghdhkd@naver.com>
2021-07-30 22:29:54 +0900

    while

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-function star(height){
-  for (let i =1 ; i<=height;i++){
-    let message='';
-    for (let star =1;star<=i;star++){
-      message+='*';
-    }console.log(message);
-  }
-}
+// while문
+// 반복문 밖에서 변수를 사용할때 나름 유용함
+let i = 1;
 
-star(3);
-star(5);
+while (i<=10){
+  console.log(`${i}번째`);
+  i++;
+}
