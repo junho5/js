ae378ed5627009a5e231208b1d84f13474f81bd2
junho5 <dhwnsghdhkd@naver.com>
2021-07-31 23:51:28 +0900

    fibonaci

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-//break continue문
+let current = 1;
+let previous = 0;
 
-let i = 1;
-
-while (i<=100){
-  console.log(`${i}`);
-  if(i===7){
-    break;
-  }
-}
-
-for (let i = 1;i<=10;i++){
-  if(i%2===0){
-    continue;
-  }console.log(i);
-}
-
-let i = 1;
-
-while(i<=10){
-  if(i%2===0){
-    i++;
-    continue;
-  }console.log(i);
-  i++;
+for (let i = 1; i<=50;i++){
+  console.log(current);
+  let temp = previous;
+  previous = current;
+  current = current + temp;
 }
