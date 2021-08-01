49ef4cb328ba45c0d08320b66c988f553009ca9d
junho5 <dhwnsghdhkd@naver.com>
2021-08-01 23:48:11 +0900

    object 2

--- object.js
+++ object.js
@@ -25,3 +25,25 @@ console.log(codeit.bestcourse.title);
 
 // 대괄호 표기법
 console.log(codeit.bestcourse['title']);
+
+
+// 객체 수정
+codeit.name = '오준호';
+console.log(codeit.name);
+
+// 객체 추가
+codeit.ceo = '전종훈';
+console.log(codeit.ceo);
+
+// 객체 삭제
+delete codeit.worstcourse;
+console.log(codeit.worstcourse);
+
+// 객체 존재 확인
+console.log('name' in codeit);
+
+if ('name' in codeit){
+  console.log(`${codeit.name}은 존재함`)
+}else{
+  console.log(`${codeit.name}은 존재하지 않음`)
+}
