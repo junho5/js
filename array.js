ce90a079eeed8cbf6e53e78b833c56f689e16af8
junho5 <dhwnsghdhkd@naver.com>
2021-08-05 00:13:35 +0900

    array 2

--- array.js
+++ array.js
@@ -1,6 +1,6 @@
 // 배열
 
-let courseRanking = [
+let array = [
   '자바스크립트 프로그래밍 기초',
   'Git으로 배우는 버전 관리',
   '컴퓨터 개론',
@@ -8,13 +8,22 @@ let courseRanking = [
 ];
 
 //indexing (0부터~~~)
+console.log(array[2]);
 
-console.log(courseRanking[2]);
+// 배열 길이 (length)
+console.log(array.length);
 
-let china = {
-  jajaman: 4000,
-  raman: 2000
-};
+// 배열 요소 추가
+array[5] = '추가된 요소';
+console.log(array[5]);
 
-console.log(china.jajaman);
-console.log(china['jajaman']);
+// 배열 요소 수정
+array[5] =  '수정된 요소';
+console.log(array[5]);
+
+// 배열 요소 삭제
+console.log(array);
+delete array[5];
+console.log(array);
+
+// 해당 방법으로 하면 배열 길이가 줄어들지 않는 문제 발생 method를 사용해야댐
