20ae1775170aaa0e855d74402ac5973348f5c846
junho5 <dhwnsghdhkd@naver.com>
2021-08-05 00:05:46 +0900

    array 1

---
+++ array.js
@@ -0,0 +1,20 @@
+// 배열
+
+let courseRanking = [
+  '자바스크립트 프로그래밍 기초',
+  'Git으로 배우는 버전 관리',
+  '컴퓨터 개론',
+  '파이썬 프로그래밍 기초'
+];
+
+//indexing (0부터~~~)
+
+console.log(courseRanking[2]);
+
+let china = {
+  jajaman: 4000,
+  raman: 2000
+};
+
+console.log(china.jajaman);
+console.log(china['jajaman']);
