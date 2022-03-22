61a7e4788033b39e7d86f46981d84e469f58b8c5
junho5 <dhwnsghdhkd@naver.com>
2022-03-22 16:58:07 +0900

    homework2

--- test.js
+++
@@ -1,9 +0,0 @@
-function login(id,password,callback){
-    setTimeout(() => {
-       console.log('사용자정보얻음');
-       callback({userId : id}) 
-    }, 3000);
-}
-const user = login('oh',123,user =>{
-    console.log(`${user.userId}`)
-})
