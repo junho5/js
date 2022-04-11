da9271ddb2835405491b071a78d1302df4084200
junho5 <dhwnsghdhkd@naver.com>
2022-04-11 23:48:03 +0900

    post finish

---
+++ homework/restFront.js
@@ -0,0 +1,79 @@
+async function getUser() { 
+    // 로딩 시 사용자 가져오는 함수
+    try {
+      const res = await axios.get('/users');
+      const users = res.data;
+      const list = document.getElementById('list');
+      list.innerHTML = '';
+
+      // 사용자마다 반복적으로 화면 표시 및 이벤트 연결
+      Object.keys(users).map(function (key) {  
+               
+        const userDiv = document.createElement('div');
+        const span = document.createElement('span');
+        span.textContent = users[key][0] +' ' + users[key][1] +' ' + users[key][2];
+
+        const edit = document.createElement('button');
+        edit.textContent = '수정';
+        edit.addEventListener('click', async () => { 
+          // 수정 버튼 클릭
+          const name = prompt('바꿀 이름을 입력하세요');
+          if (!name) {
+            return alert('이름을 반드시 입력하셔야 합니다');
+          }
+          try {
+            await axios.put('/user/' + key, { name });
+            getUser();
+          } catch (err) {
+            console.error(err);
+          }
+        });
+
+        const remove = document.createElement('button');
+        remove.textContent = '삭제';
+        remove.addEventListener('click', async () => { 
+          // 삭제 버튼 클릭
+          try {
+            await axios.delete('/user/' + key);
+            getUser();
+          } catch (err) {
+            console.error(err);
+          }
+        });
+        userDiv.appendChild(span);
+        userDiv.appendChild(edit);
+        userDiv.appendChild(remove);
+        list.appendChild(userDiv);
+        console.log(res.data);
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
+  window.onload = getUser; // 화면 로딩 시 getUser 호출
+  // 폼 제출(submit) 시 실행
+  document.getElementById('form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const name = e.target.username.value;
+    const price = e.target.price.value;
+    const stock = e.target.stock.value;
+    if (!name) {
+      return alert('이름을 입력하세요');
+    }
+    if (!price) {
+        return alert('가격을 입력하세요');
+      }
+    if (!stock) {
+      return alert('재고유무를 입력하세요');
+    }  
+    try {
+      await axios.post('/user', { name,price,stock });
+      getUser();
+    } catch (err) {
+      console.error(err);
+    }
+    e.target.username.value = '';
+    e.target.price.value = '';
+    e.target.stock.value = '';
+  });
---
+++ homework/restServer.js
@@ -0,0 +1,95 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+const users = {}; // 데이터 저장용
+const port = 8080;
+
+const restServer = http.createServer(async (req, res) => {
+  try {
+    if (req.method === 'GET') {
+      // GET 요청
+      if (req.url === '/') {
+        const data = await fs.readFile('./restFront.html');
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        return res.end(data);
+      } else if (req.url === '/about') {
+        const data = await fs.readFile('./about.html');
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        return res.end(data);
+      } else if (req.url === '/users') {
+        res.writeHead(200, { 
+            'Content-Type': 'application/json; charset=utf-8', 
+        });
+        res.end(JSON.stringify(users));
+      }else{
+      // 기타 다른 자원을 요청하는 경우 (image, css, javascript files)
+      try {
+        const data = await fs.readFile(`.${req.url}`);
+        res.end(data);
+      } catch (err) {
+        // 주소에 해당하는 라우트를 못 찾았다는 404 Not Found error 발생
+        console.error(err);
+        res.writeHead(404,{'Content-Type': 'text/html; charset=utf-8'});
+        res.end('NOT FOUND');
+      }
+    }
+    } else if (req.method === 'POST') {
+      // POST 요청
+      if (req.url === '/user') {
+        req.on('data', (data) => {
+            console.log('POST 본문(Body):', data.toString());
+            const { name } = JSON.parse(data);
+            const { price } = JSON.parse(data);
+            const { stock } = JSON.parse(data);
+            const id = Date.now();
+            users[id] = [name,price,stock];
+            res.writeHead(201, { 'Content-Type': 'text/plain; charset=utf-8' });
+            res.end('ok');
+        });
+      } else{
+          console.error(err);
+          res.statusCode = 404;
+          res.end('NOT FOUND');
+      }
+    } else if (req.method === 'PUT') {
+      // PUT 요청
+      if (req.url.startsWith('/user/')) {
+        const key = req.url.split('/')[2];
+        req.on('data', (data) => {
+          console.log('PUT 본문(Body):', data.toString());
+          users[key] = JSON.parse(data).name;
+          res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+          res.end('ok');
+        });
+      }else{
+        console.error(err);
+        res.statusCode = 404;
+        res.end('NOT FOUND');
+    }
+    } else if (req.method === 'DELETE') {
+      // DELETE 요청
+      if (req.url.startsWith('/user/')) {
+        const key = req.url.split('/')[2];
+        delete users[key];
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+        return res.end('ok');
+      }else{
+        console.error(err);
+        res.statusCode = 404;
+        res.end('NOT FOUND');
+        }
+    } else{
+        console.error(err);
+        res.statusCode = 400;
+        res.end('BAD REQUEST');
+    }
+  } catch (err) {
+    console.error(err);
+    res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end(err.message);
+  }
+})
+
+restServer.listen(port, () => {
+    console.log('8080번 포트에서 서버 대기 중입니다');
+  });
