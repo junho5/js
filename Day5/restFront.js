2aad6b064e9bd082b368e01de6f302e885ab2e62
junho5 <dhwnsghdhkd@naver.com>
2022-04-11 18:42:10 +0900

    homework5

--- Day5/restFront.js
+++ Day5/restFront.js
@@ -1,9 +1,11 @@
-async function getUser() { // 로딩 시 사용자 가져오는 함수
+async function getUser() { 
+    // 로딩 시 사용자 가져오는 함수
     try {
       const res = await axios.get('/users');
       const users = res.data;
       const list = document.getElementById('list');
       list.innerHTML = '';
+
       // 사용자마다 반복적으로 화면 표시 및 이벤트 연결
       Object.keys(users).map(function (key) {
         const userDiv = document.createElement('div');
@@ -11,7 +13,8 @@ async function getUser() { // 로딩 시 사용자 가져오는 함수
         span.textContent = users[key];
         const edit = document.createElement('button');
         edit.textContent = '수정';
-        edit.addEventListener('click', async () => { // 수정 버튼 클릭
+        edit.addEventListener('click', async () => { 
+          // 수정 버튼 클릭
           const name = prompt('바꿀 이름을 입력하세요');
           if (!name) {
             return alert('이름을 반드시 입력하셔야 합니다');
@@ -23,9 +26,11 @@ async function getUser() { // 로딩 시 사용자 가져오는 함수
             console.error(err);
           }
         });
+
         const remove = document.createElement('button');
         remove.textContent = '삭제';
-        remove.addEventListener('click', async () => { // 삭제 버튼 클릭
+        remove.addEventListener('click', async () => { 
+          // 삭제 버튼 클릭
           try {
             await axios.delete('/user/' + key);
             getUser();
