58ef1e1d8cf42f3224fa2a5cd94e18131409ac32
junho5 <dhwnsghdhkd@naver.com>
2022-04-14 22:24:42 +0900

    cookie

---
+++ Day6/cookie.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+  console.log(req.url, req.headers.cookie);
+  res.writeHead(200, { 'Set-Cookie': ['class=Advweb', 'credits=3'] });
+  res.write('Cookie testing')
+  res.end('.........');
+})
+  .listen(3000, () => {
+    console.log('3000번 포트에서 서버 대기 중입니다!');
+  });
