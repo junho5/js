bb7993602ec0216bbcd3d146d0a332a41bbfc55d
junho5 <dhwnsghdhkd@naver.com>
2022-05-02 10:04:57 +0900

    week 9 multer

--- Day8/routes/upload_v1.js
+++ Day8/routes/upload_v1.js
@@ -3,20 +3,12 @@
 
 const express = require('express');
 const path = require('path');
-const multer = requre('multer');
 
 const router = express.Router();
-
-const upload = multer({ dest: 'uploads/'});
 
 router.get('/',(req, res) => {
     res.sendFile(path.join(__dirname, '../public/upload.html'))
 })
-
-router.post('/', upload.single('image'),(req,res)=>{
-    console.log(req.file, req.body);
-    res.send('Upload!');
-});
 
 module.exports = router;
 
