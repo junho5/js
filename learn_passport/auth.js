2d9b39c0a8079ca5eeb4790a8edcbfe519abd3d5
junho5 <dhwnsghdhkd@naver.com>
2022-05-25 10:14:12 +0900

    nodebird v1

--- learn_passport/auth.js
+++ learn_passport/auth.js
@@ -1,32 +1,42 @@
+
 const express = require('express');
 const path = require('path');
 const passport = require('./passportmiddleware');
 
 const router = express.Router();
 
+// passport 에서 생성한 req.user로 로그인확인
 router.get('/', (req, res) => {
-  // passport 에서 생성한 req.user로 로그인확인
-  if (req.user) {
-    res.send(`${req.user.username}(${req.user.email})님 로그인되었습니다!`);
-  } else {
+if (req.user) {
+    const html = `
+    <p>${req.user.username}(${req.user.email})님 로그인되었습니다!</p>
+    <a href="/logout">로그아웃</a>
+    `;
+    res.send(html);
+} else {
     res.redirect('/login');
-  }
+}
 });
 
 router.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/login.html'));
+res.sendFile(path.join(__dirname, './public/login.html'));
 });
 
 /* Passport Login */
-/* Local-Strategy를 이용해서 사용자 인증 요청 처리 */
 router.post(
-  '/login',
-  passport.authenticate('local', {
+'/login',
+passport.authenticate('local', {
     failureRedirect: '/login',
-  }),
-  (req, res) => {
+}),
+(req, res) => {
     res.redirect('/');
-  }
+}
 );
+
+router.get('/logout', (req, res) => {
+req.logout(() => {
+    res.redirect('/');
+});
+});
 
 module.exports = router;
